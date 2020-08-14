1eb823ca983624bb17eedfd5413e9379c912c811
Mike J McGuire <mike@mcguire.mx>
2020-08-14 16:41:19 +1000

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,8 +5,16 @@ if [ -z "$DATA_MOUNT" ]; then
   exit -1
 fi
 
-if [ -z "$(ls -A $DATA_MOUNT)" ]; then
+if ! grep -qs $DATA_MOUNT /proc/mounts; then
   echo "Waiting for mount..."
+  sleep 5
+  exit -1
+fi
+
+sleep 5
+
+if [ -z "$(ls -A $DATA_MOUNT)" ]; then
+  echo "Waiting for files..."
   sleep 5
   exit -1
 fi
